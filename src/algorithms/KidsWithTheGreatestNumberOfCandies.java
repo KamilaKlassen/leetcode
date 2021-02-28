4cff1c3f73def63654b2494ce1e485592f6610f4
Kamila Klassen <kamilkavltv@icloud.com>
2021-02-28 14:00:56 +0100

    Solution of 1431

---
+++ src/algorithms/KidsWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,25 @@
+package algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KidsWithTheGreatestNumberOfCandies {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> list = new ArrayList<>();
+        int max = 0;
+
+        for (int i : candies) {
+            if (i > max) {
+                max = i;
+            }
+        }
+        for (int candy : candies) {
+            if (candy + extraCandies >= max) {
+                list.add(true);
+            } else {
+                list.add(false);
+            }
+        }
+        return list;
+    }
+}
